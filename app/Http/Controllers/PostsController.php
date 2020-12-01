6bfa4e2a038c4bd0171c22a8a50b765366fbb652
3A732080 <tiffany89031176@gmail.com>
2020-12-01 19:02:38 +0800

    練習8-1：修改(PostsController@index)

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Post;
 
 class PostsController extends Controller
 {
     public function index()
     {
+        $posts = Post::orderBy('created_at','DESC')->get();
+        $data = [
+            'posts' => $posts,
+        ];
         return view('posts.index');
     }
 
